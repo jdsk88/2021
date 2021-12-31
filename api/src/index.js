f5c30659ba49188a48b3a637b373d01b92bbb5a0
jdsk88 <maciejjakobszy@me.com>
2021-12-31 08:27:27 +0100

    api .env updated && give privilages to use port below 1024

--- api/src/index.js
+++ api/src/index.js
@@ -8,12 +8,27 @@ import { options } from "./config/cors.js";
 import fs from "fs";
 import https from "https";
 import routes from "./routes/index.js";
+import rfs from "rotating-file-stream";
+import path from "path";
 
 const app = express();
-app.use(morgan(logger));
+const __dirname = path.resolve();
+const rfsStream = rfs.createStream("log.txt", {
+  size: "10M",
+  interval: "1d",
+  compress: "gzip",
+});
+rfs.createStream("logs.json", { size: "10M", interval: "1d", mode: "" });
+
+app.use(morgan(logger, { stream: rfsStream }));
 app.use(errorhandler());
 app.use(cors(options));
-app.use("/images", express.static("./public/uploads"));
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.get("/", (req, res) => {
+  res.send({ statusbar });
+});
+app.use("/api", express.json());
+app.use("/api/", routes);
 app.use("/api", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", `*`);
   res.setHeader("Content-Type", "application/json");
@@ -23,20 +38,15 @@ app.use("/api", function (req, res, next) {
   );
   next();
 });
-app.use("/api", express.json());
-app.get("/", (req, res) => {
-  res.send({ statusbar });
-});
-app.use("/api/", routes);
+app.use("/images", express.static("./public/uploads"));
 
-app.use("*", (req, res) => {
-  res.status(404).json({
-    success: req.complete,
-    message: `Cannot /${req.method} ${req.protocol}://${req.hostname}:${PORT2}${req.originalUrl} from ${req.ip}`,
-    error: {
-      statusCode: res.statusCode,
-      message: `Not found path ${req.originalUrl}`,
-    },
+app.use("/*", function (req, res) {
+  const url = __dirname.replace("/api", "");
+  console.log(`${url}/build/index.html`);
+  res.sendFile(`${url}/build/index.html`, function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
   });
 });
 
--- api/src/routes/crypto.js
+++ api/src/routes/crypto.js
@@ -1,10 +1,13 @@
 import express from "express";
 import CoinGecko from "coingecko-api";
+import { CryptoServices } from "../services/crypto/index.js";
 
 const CoinGeckoClient = new CoinGecko();
 let data = await CoinGeckoClient.coins.all();
 let currencies = [];
 let symbols = [];
+let lineChartData = [];
+
 data.data.forEach((element) => {
   currencies.push({
     id: element.id,
@@ -19,9 +22,19 @@ data.data.forEach((element) => {
 data.data.forEach((element) => {
   symbols.push(element.symbol);
 });
+data.data.forEach((element) => {
+  lineChartData.push({
+    name: element.name,
+    price: element.market_data.current_price.usd,
+    timeline: element.market_data.current_price.usd,
+  });
+});
 
 const routes = express.Router({});
-
+routes.get("/database", async (req, res) => {
+  const result = await CryptoServices.getCryptoData();
+  res.send(result);
+});
 routes.get("/currencies", async (req, res) => {
   res.send(symbols);
 });
@@ -33,13 +46,19 @@ routes.get("/currencies/:currency", async (req, res) => {
   const item = data.data.find((element) => element.symbol === currency);
   res.send(item);
 });
+routes.get("/currencies/widgetdata/lineChart", async (req, res) => {
+  res.send(lineChartData);
+});
+
 routes.get("/currencies/widgetdata/:currency", async (req, res) => {
   const { currency } = req.params;
   const item = data.data.find((element) => element.symbol === currency);
   const widgetdata = {
     symbol: item.symbol,
-    current: Object.keys(item.market_data.current_price).map(
-      (key) => item.market_data.current_price[key]
+    current: Object.keys(item.market_data.current_price).map((key) =>
+      item.market_data.current_price[key] <= 10000
+        ? item.market_data.current_price[key]
+        : null
     ),
 
     high24h: Object.keys(item.market_data.high_24h).map(
@@ -51,8 +70,33 @@ routes.get("/currencies/widgetdata/:currency", async (req, res) => {
     ),
 
     exchange: Object.keys(item.market_data.current_price).map((key) => key),
+    name: item.name,
+    price: item.market_data.current_price,
   };
   res.send(widgetdata);
 });
+
+// routes.post("/create", async (req, res) => {
+//   const {
+//     symbol,
+//     name,
+//     block_time_in_minutes,
+//     image,
+//     market_data,
+//     last_updated,
+//     localization,
+//   } = req.body;
+//   const result = await CryptoServices.createCryptoData({
+//     symbol,
+//     name,
+//     block_time_in_minutes,
+//     image,
+//     market_data,
+//     last_updated,
+//     localization,
+//   });
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.send(result);
+// });
 
 export default routes;
