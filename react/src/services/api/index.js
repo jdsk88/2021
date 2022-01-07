c4ca84e566c05c1a80940f5ab068d8c82e1bc063
jdsk88 <maciejjakobszy@me.com>
2022-01-07 10:16:25 +0100

    fixing fucing ips and add build

--- react/src/services/api/index.js
+++ react/src/services/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const remote = "http://85.222.120.170:8888/api";
 const local = "http://192.168.0.24:8888/api";
 const api = axios.create({
-  baseURL: local,
+  baseURL: remote,
 });
 
 export default api;
